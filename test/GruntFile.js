4801f477266ae645f55f393c706bf358a17d454f
Tyler Beck <tyler@tylerbeck.com>
2014-03-28 15:10:57 -0400

    Moved task logic into external classes

---
+++ test/GruntFile.js
@@ -0,0 +1,33 @@
+/***********************************************************************
+ * Default Grunt Scaffolding
+ * Author: Copyright 2012-2014, Tyler Beck
+ * License: MIT
+ ***********************************************************************/
+
+var gtools = require('../lib/index');
+
+var configDirs = [
+
+];
+
+var taskDirs = [
+	"../lib/tasks"
+];
+
+//npmTasks, taskDirectories, configDirectories, initFn
+module.exports = new gtools.Scaffold(
+		//load npm tasks
+		true,
+		//array of or single directory path in which grunt tasks have been defined
+		taskDirs,
+		//array of or single directory path in which grunt configuration objects have been defined
+		configDirs,
+		//grunt file scripts
+		function( grunt ){
+
+			grunt.registerTask('default', function(){
+				grunt.log.writeln( 'IT WORKS!' );
+			})
+
+		}
+);
